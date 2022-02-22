252219334d6b1fb46184e30e7310531ea97d4173
brusooo <jenifar200227@gmail.com>
2022-02-22 14:53:52 +0530

    Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import "./App.css"
 import Container from './components/Container'
 
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 3000)
+  })
+
   return (
     <div>
-      <div className='title'>
-        <h1>Do and Get it</h1>
+      {!loading ? 
+      
+      <div class="loader">
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+      
+      :
 
-      <Container />
+        <div>
+          <div className='title'>
+            <h1>Do and Get it</h1>
+          </div>
 
-      <div className='footer'>
-        <h1>Brusooo</h1>
-      </div>
+          <Container />
 
+          <div className='footer'>
+            <h1>Brusooo</h1>
+          </div>
+        </div>
+      }
     </div>
   )
 }
